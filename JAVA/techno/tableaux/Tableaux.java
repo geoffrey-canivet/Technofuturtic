02852f93e1d795747d94b1760bbcba38ff8e25be
geoffreycani <geoffrey.canivet@gmail.com>
2024-10-14 17:15:48 +0200

    exo

--- JAVA/techno/tableaux/Tableaux.java
+++ JAVA/techno/tableaux/Tableaux.java
@@ -7,7 +7,7 @@ public class Tableaux {
     
     public static void main(String[] args) {
 //        exo1();
-//        exo2();
+        exo2();
 //        exo3();
     };
 
@@ -21,7 +21,53 @@ public class Tableaux {
         System.out.println("Tableau modifié : " + Arrays.toString(tableEntier));
     };
 
-    static void exo2(){
+    static void exo2() {
+
+        int userNb = 4;
+        int size = 2 * userNb + 1;
+
+        int[][] tableauMulti = new int[size][size];
+
+        // remplire le tableau
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                tableauMulti[i][j] = 1;
+
+            }
+        }
+
+        // affiche le tableau
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(tableauMulti[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+
+    static void exo3(){
+        String[] tabString = new String[100];
+
+        for (int i = 0; i < tabString.length; i++) {
+            if (i %5 == 0 && i %7 == 0) {
+                tabString[i] = "Ding-Ding Bottle";
+            } else if (i %5 == 0){
+                tabString[i] = "Ding-Ding";
+            } else if (i %7 == 0) {
+                tabString[i] = "Bottle ";
+            } else {
+                tabString[i] = Integer.toString((i));
+            }
+        }
+        System.out.println("Tableau modifié : " + Arrays.toString(tabString));
+    }
+
+
+
+    static void exo2bis(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Entrez un nombre : ");
         int n = scanner.nextInt();  // Le nombre que l'utilisateur entre
@@ -45,33 +91,12 @@ public class Tableaux {
         // Affichage du tableau
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                System.out.print(tableau[i][j] + " ");
+                System.out.print(tableau[i][j] + "test");
             }
             System.out.println();  // Pour aller à la ligne après chaque rangée
         }
 
         scanner.close();  // Fermer le scanner
     }
-
-    static void exo3(){
-        String[] tabString = new String[100];
-
-        for (int i = 0; i < tabString.length; i++) {
-            if (i %5 == 0 && i %7 == 0) {
-                tabString[i] = "Ding-Ding Bottle";
-            } else if (i %5 == 0){
-                tabString[i] = "Ding-Ding";
-            } else if (i %7 == 0) {
-                tabString[i] = "Bottle ";
-            } else {
-                tabString[i] = Integer.toString((i));
-            }
-        }
-        System.out.println("Tableau modifié : " + Arrays.toString(tabString));
-    }
-
-
-
-
 
 }
